d9f6da549390ab084e74ea0175ae999f0aba181d
Tan <tannb@sendo.vn>
2020-09-24 08:50:19 +0700

    Initial Commit

---
+++ NetworkTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkTemplate
+//
+//  Created by Nguyen Ba Tan - DC3 on 9/24/20.
+//  Copyright © 2020 Nguyen Ba Tan - DC3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

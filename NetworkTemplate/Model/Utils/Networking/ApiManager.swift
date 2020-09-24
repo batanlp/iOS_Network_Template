d816c11d5cafa120f0f32900a6755a4d0209243c
Tan <tannb@sendo.vn>
2020-09-24 09:28:55 +0700

    first commit

---
+++ NetworkTemplate/Model/Utils/Networking/ApiManager.swift
@@ -0,0 +1,13 @@
+//
+//  ApiManager.swift
+//  NetworkTemplate
+//
+//  Created by Nguyen Ba Tan - DC3 on 9/24/20.
+//  Copyright © 2020 Nguyen Ba Tan - DC3. All rights reserved.
+//
+
+import UIKit
+
+class ApiManager: NSObject {
+
+}
---
+++ NetworkTemplate/Model/Utils/Networking/Networking.swift
@@ -0,0 +1,13 @@
+//
+//  Networking.swift
+//  NetworkTemplate
+//
+//  Created by Nguyen Ba Tan - DC3 on 9/24/20.
+//  Copyright © 2020 Nguyen Ba Tan - DC3. All rights reserved.
+//
+
+import UIKit
+
+class Networking: NSObject {
+
+}
